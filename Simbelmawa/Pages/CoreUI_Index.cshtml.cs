18f529f1b31350a88bd8ddefdd34463185ba5e88
Fathoni <m.fathoni@mail.com>
2020-10-24 08:44:05 +0700

    memasukkan css CoreUI

---
+++ Simbelmawa/Pages/CoreUI_Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Simbelmawa.Pages
+{
+    public class CoreUI_IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
